956f247a296ec5a3a269d0b602f3e118eab26a1b
halezmarie <teacherhaleymarie@gmail.com>
2021-11-03 14:17:25 -0500

    Added toy model

---
+++ db/migrate/20211103191516_create_toys.rb
@@ -0,0 +1,12 @@
+class CreateToys < ActiveRecord::Migration[6.1]
+  def change
+    create_table :toys do |t|
+      t.string :name
+      t.string :description
+      t.string :category
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
