29fd4de087180c765a80576e776415344ca23354
halezmarie <teacherhaleymarie@gmail.com>
2021-11-08 13:49:06 -0600

    Fixed the index action along with the create action - need to add serializers of course

--- app/controllers/api/v1/toys_controller.rb
+++ app/controllers/api/v1/toys_controller.rb
@@ -1,19 +1,26 @@
 class Api::V1::ToysController < ApplicationController
 
-
+    # when I make the fetch request and come into this action I will want to be getting all of the toys. 
+    # I will then send these back to the frontend with JSON. 
     def index 
+        @toys = Toy.all
+        render json: @toys
 
+    end
 
+    # Use new to pass through the instance through the toy params
+    def create
+        @toy = Toy.new
+        if @toy.save
+            render json: @Toy
+        else
+            render json: {error: "OH NO😦! There was an error when you tried creating the toy. Try again 😁!"}
+        end
     end
 
     def show
 
     end
-
-    def create
-
-    end
-
 
     private
 
