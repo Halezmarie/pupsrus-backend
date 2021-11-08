6d0fd03bf45e2ce73c6365746f86c1af09d0588a
halezmarie <teacherhaleymarie@gmail.com>
2021-11-08 13:41:24 -0600

    Made actions for my controllers

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::ReviewsController < ApplicationController
+
+    def index 
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :content, :rating, :toy_id)
 end
--- app/controllers/api/v1/toys_controller.rb
+++ app/controllers/api/v1/toys_controller.rb
@@ -1,6 +1,27 @@
 class Api::V1::ToysController < ApplicationController
 
 
+    def index 
+
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+
+    private
+
+    def toy_params
+        params.require(:toy).permit(:name, :description, :image_url, :toy_id)
+end
+
+
 
 
     
