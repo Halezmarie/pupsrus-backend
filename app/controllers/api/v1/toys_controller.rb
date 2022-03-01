09bfa0b054dbb4183292055eb15f17c6b02983b2
Halezmarie <teacherhaleymarie@gmail.com>
2022-02-28 21:55:48 -0600

    Cleaning up code

--- app/controllers/api/v1/toys_controller.rb
+++ app/controllers/api/v1/toys_controller.rb
@@ -2,15 +2,12 @@ class Api::V1::ToysController < ApplicationController
     before_action :set_toy, only: [:show, :update, :destroy]
     # data manipulation
    
-    # api/v1/toys/1/reviews 
-    # when I make the fetch request and come into this action I will want to be getting all of the toys. 
-    # I will then send these back to the frontend with JSON. 
     def index 
         @toys = Toy.all.sort_by {|obj| obj.name}
         render json: @toys
     end
 
-    # Use new to pass through the instance through the toy params
+
     def create
         @toy = Toy.new(toy_params)
         if @toy.save
@@ -20,7 +17,7 @@ class Api::V1::ToysController < ApplicationController
         end
     end
 
-    # These params are looking for /api/v1/toys/1 <- that toy id
+ 
     def show
         @toy = Toy.find(params[:id])
         render json: @toy
