ef8b7496209d15ee9a771b0d8434037a4aeb859b
Halezmarie <teacherhaleymarie@gmail.com>
2022-02-28 22:01:51 -0600

    Formatting values for the reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
     belongs_to :toy
     validates :title, :content, :rating, presence: true
+
+    def format_values
+        self.title = self.title.titleize.strip
+        self.content = self.content.capitalize
+      end
 end
