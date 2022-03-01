b3a5381c9a7744e6aa040482ca1d0a63ade77821
Halezmarie <teacherhaleymarie@gmail.com>
2022-02-28 21:59:34 -0600

    Formatting values

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,4 +1,10 @@
 class Toy < ApplicationRecord
     has_many :reviews #dependent on destroy 
     validates :name, :description, :image_url, presence: true
+
+    def format_values
+        self.name = self.name.titleize.strip
+        self.description = self.description.capitalize
+      end
+
 end
