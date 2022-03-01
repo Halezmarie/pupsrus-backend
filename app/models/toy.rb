1df9cb9042b92bffb7e0a6853dc1d847ea4fcba7
Halezmarie <teacherhaleymarie@gmail.com>
2022-02-28 22:00:17 -0600

    Added valiations for not duplicating names of toys

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,6 +1,10 @@
 class Toy < ApplicationRecord
     has_many :reviews #dependent on destroy 
     validates :name, :description, :image_url, presence: true
+    
+    
+    validate :not_a_duplicate
+    before_validation :format_values
 
     def format_values
         self.name = self.name.titleize.strip
