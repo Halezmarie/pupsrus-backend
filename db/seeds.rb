25b6bf344fb746f2ce1bf761748c4ba938b4ff20
Halezmarie <teacherhaleymarie@gmail.com>
2022-03-07 18:55:34 -0600

    Added cactus toy

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,6 @@ orangeball = Toy.create(name: "Orange  Ball", description: "An orange ball that 
 
 wobblewaggiggle = Toy.create(name: "Giggle Toy", description: "A ball that giggles ", image_url: "https://m.media-amazon.com/images/I/81GGDYhfSCL._AC_SX466_.jpg")
 
+cactustoy = Toy.create(name: "Cactus Toy", description: "Cactus toy for dogs to play with", image_url: "https://m.media-amazon.com/images/I/71VmbDpjhQL._AC_SX466_.jpg")
+
 Review.create(title: "My pup loves this!", content: "Our baby Sparkles loves this teddy bear, I am so happy she is gentle with it!", rating: "10", toy_id: 1)
